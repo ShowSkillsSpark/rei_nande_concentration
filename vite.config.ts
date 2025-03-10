540b95bed46c7cb3cff3767fc6e87d08690fa943
ShowSkillsSpark <sparkamita90.yt@gmail.com>
2025-03-10 17:45:58 +0900

    initial commit

---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "vite";
+
+// https://vite.dev/config/
+export default defineConfig({
+  server: {
+    port: 8080,
+    open: true,
+    watch: {
+      usePolling: true,
+      interval: 1000,
+    }
+  },
+});
